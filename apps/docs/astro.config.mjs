32b85192425e0072b92fbf52528c44afd5e857c5
Yanick Rochon <yanick.rochon@gmail.com>
2024-03-21 16:01:04 -0400

    feat: add github page deployment

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), react()]
+  integrations: [mdx(), tailwind(), react()],
+  site: 'https://yanickrochon.github.io',
+  base: 'i18n',
 });
